ed8d82a9991d0a327741575ae4b84a73deec849e
Masaharu41 <dalekrebel@gmail.com>
2024-10-11 13:02:46 -0600

    Created simple write to serial port

--- EUART SERIAL/EUART SERIAL/EuartForm.vb
+++ EUART SERIAL/EUART SERIAL/EuartForm.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO.Ports
+Imports System.Runtime.InteropServices
 
 Public Class EuartForm
     Private Sub SerialCom_Load(sender As Object, e As EventArgs) Handles Me.Load
@@ -7,7 +8,26 @@ Public Class EuartForm
         SerialPort.Open()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+    Private Sub SendButton_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+        SendData()
+    End Sub
+
+    Sub SendData()
+        Dim pulse% = PulseWidth()
+        Dim data(1) As Byte
+        data(0) = &H24
+        data(1) = Hex(pulse)
+
+        SerialPort.Write(data, 0, 2)
+
 
     End Sub
+
+
+    Function PulseWidth() As Integer
+        Dim pulse As Integer
+        pulse = CInt(PwTrackBar.Value) * 6 + 32
+        Return pulse
+    End Function
+
 End Class
