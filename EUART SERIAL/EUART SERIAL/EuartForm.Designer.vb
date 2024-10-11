2daec5c1c6454138e85c1491a8586d5e2612e49a
Masaharu41 <dalekrebel@gmail.com>
2024-10-11 12:51:48 -0600

    Created new project

---
+++ EUART SERIAL/EUART SERIAL/EuartForm.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EuartForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.PwTrackBar = New System.Windows.Forms.TrackBar()
+        Me.SendButton = New System.Windows.Forms.Button()
+        CType(Me.PwTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PwTrackBar
+        '
+        Me.PwTrackBar.Location = New System.Drawing.Point(35, 36)
+        Me.PwTrackBar.Maximum = 20
+        Me.PwTrackBar.Name = "PwTrackBar"
+        Me.PwTrackBar.Size = New System.Drawing.Size(742, 90)
+        Me.PwTrackBar.TabIndex = 0
+        '
+        'SendButton
+        '
+        Me.SendButton.Location = New System.Drawing.Point(35, 122)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(155, 46)
+        Me.SendButton.TabIndex = 1
+        Me.SendButton.Text = "Send"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'EuartForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 583)
+        Me.Controls.Add(Me.SendButton)
+        Me.Controls.Add(Me.PwTrackBar)
+        Me.Name = "EuartForm"
+        Me.Text = "Form1"
+        CType(Me.PwTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents PwTrackBar As TrackBar
+    Friend WithEvents SendButton As Button
+End Class
---
+++ EUART SERIAL/EUART SERIAL/EuartForm.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO.Ports
+
+Public Class EuartForm
+    Private Sub SerialCom_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SerialPort.PortName = "COM5"
+        SerialPort.BaudRate = 9600
+        SerialPort.Open()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SendButton.Click
+
+    End Sub
+End Class
